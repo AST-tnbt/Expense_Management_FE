be8c3d60375c6f717f71f3cdf1c9fe941f4ffe30
vinh0605 <voducvinh28@gmail.com>
2025-04-25 10:37:03 +0700

    create login, register. add font inter

--- app/src/main/java/com/example/expense_management/MainActivity.java
+++ app/src/main/java/com/example/expense_management/MainActivity.java
@@ -1,12 +1,13 @@
 package com.example.expense_management;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.textview.MaterialTextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,11 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(R.layout.login);
+        MaterialTextView registerHere = findViewById(R.id.registerHere);
+
+        registerHere.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, register.class);
+                startActivity(intent);
+            }
         });
+
+
     }
 }
